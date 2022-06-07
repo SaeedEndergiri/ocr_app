8014925fd8ee1fd56d3ca1d1335a47a52ec56f50
Saeed Endergiri <saeeda.endergiri@outlook.com>
2022-06-07 16:26:42 +0300

    basic functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +11,211 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: OcrScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class OcrScreen extends StatefulWidget {
+  const OcrScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<OcrScreen> createState() => _OcrScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _OcrScreenState extends State<OcrScreen> {
+  File? _storedImage;
+  String? resultText = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _scanImage() async {
+    String _ocrResult =
+        await FlutterTesseractOcr.extractText(_storedImage!.path, args: {
+      "psm": "4",
+      "preserve_interword_spaces": "1",
     });
+    setState(() {
+      resultText = _ocrResult;
+    });
+  }
+
+  Future<void> _takePicture(ImageSource src) async {
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
+      source: src,
+      maxWidth: 600,
+    );
+    try {
+      if (imageFile == null) {
+        return;
+      }
+      setState(() {
+        _storedImage = File((imageFile).path);
+      });
+    } catch (e) {
+      // TODO
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: const Text('OCR Scanner')),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 350,
+                margin: const EdgeInsets.only(
+                    top: 5, right: 10, left: 10, bottom: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                child: _storedImage == null
+                    ? const Center(
+                        child: Text(
+                          'No Image Uploaded Yet',
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    : FittedBox(
+                        child: Image.file(
+                          _storedImage as File,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+              ),
+              if (_storedImage != null)
+                TextButton(
+                  onPressed: _scanImage,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.cyan, primary: Colors.white),
+                  child: const Text('Scan image'),
+                ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Result: '),
+                    IconButton(
+                      icon: const Icon(Icons.copy),
+                      color: Colors.grey,
+                      onPressed: () =>
+                          Clipboard.setData(ClipboardData(text: resultText))
+                              .then(
+                        (value) {
+                          //only if ->
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                'Text added to clipboard',
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ); // -> show a notification
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                padding: const EdgeInsets.all(5),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: SingleChildScrollView(child: Text(resultText ?? '')),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        //padding: EdgeInsets.all(10),
+                        constraints: const BoxConstraints(
+                          minWidth: 75,
+                          minHeight: 75,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: IconButton(
+                          padding: const EdgeInsets.all(0),
+                          onPressed: () {
+                            _takePicture(ImageSource.camera);
+                          },
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            size: 50,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      const Text('Take a picture!'),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        //padding: EdgeInsets.all(10),
+                        constraints: const BoxConstraints(
+                          minWidth: 75,
+                          minHeight: 75,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.grey,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: IconButton(
+                          padding: const EdgeInsets.all(0),
+                          onPressed: () {
+                            _takePicture(ImageSource.gallery);
+                          },
+                          icon: const Icon(
+                            Icons.image_outlined,
+                            size: 50,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      const Text('Choose from Gallery!'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
